fcbfb48f1495c42ff869b5bd9992a8bc8eb19ba8
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-05-28 18:20:45 +0200

    Snack-2

--- src/App.java
+++ src/App.java
@@ -2,8 +2,32 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         // creazione di uno studente
-        Studente Studente = new Studente("gioVAnNI", "PASCALI", "20");
-        System.out.println(Studente.ConcatData());
+        System.out.println("Snack 1: Studente");
+        Studente StudenteGiovanni = new Studente("gioVAnNI", "PASCALI", "20");
+        System.out.println(StudenteGiovanni.ConcatData());
+        System.out.println("----------------------");
+        System.out.println("----------------------");
+
+        System.out.println("Snack 2: Conto Bancario");
+        // creazione di un conto bancario
+        ContoBancario ContoGiovanni = new ContoBancario("gvn-6548");
+        // deposito di 4000 euro
+        ContoGiovanni.Deposita(4000);
+        // visualizzazione del saldo
+        System.out.println("Numero Conto: " + ContoGiovanni.getNumeroConto() + " Saldo corrente: "
+                + ContoGiovanni.VisualizzaSaldo() + " euro");
+        // prelievo di 1340 euro
+        ContoGiovanni.Preleva(1340);
+        // visualizzazione del saldo dopo il prelievo
+        System.err.println("A seguire il saldo dopo un prelievo di 1340 euro ");
+        ;
+        System.out.println("Numero Conto: " + ContoGiovanni.getNumeroConto() + " Saldo corrente: "
+                + ContoGiovanni.VisualizzaSaldo() + " euro");
+        // tentativo di prelievo di 5000 euro
+        System.err.println("A seguire il tentativo di prelievo di 5000 euro: ");
+        ContoGiovanni.Preleva(5000);
+        System.out.println("----------------------");
+        System.out.println("----------------------");
 
     }
 }
--- src/ContoBancario.java
+++ src/ContoBancario.java
@@ -1,3 +1,39 @@
 public class ContoBancario {
+    // attributi della classe ContoBancario
+    private String numeroConto;
+    private Double saldo;
+
+    // metodo costruttore
+    public ContoBancario(String numeroConto) {
+        this.numeroConto = numeroConto;
+        this.saldo = 0.0;
+    }
+
+    // metodo per restituire il numero del conto
+    public String getNumeroConto() {
+        return this.numeroConto;
+    }
+
+    // metodo per depositare sul conto
+    public void Deposita(int importo) {
+        this.saldo += importo;
+
+    }
+
+    // metodo per prelevare dal conto
+    public void Preleva(int importo) {
+        if (importo <= this.saldo) {
+            this.saldo -= importo;
+        } else {
+            System.out
+                    .println("Il tuo saldo di: " + this.saldo + " euro non è sufficiente per prelevare l'importo di: "
+                            + importo + " euro richiesto.");
+        }
+    }
+
+    // metodo per visualizzare il saldo corrente
+    public Double VisualizzaSaldo() {
+        return this.saldo;
+    }
 
 }
